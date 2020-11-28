8665610fc673205a0ce04126eed81ee5515eddcf
Prüffer <pruffer@avantheim.org>
2020-11-28 13:45:39 +0100

    moved some functions to varfunctions.php

--- Upload/inc/plugins/restfulapi/functions/stringfunctions.php
+++ Upload/inc/plugins/restfulapi/functions/stringfunctions.php
@@ -1,14 +1,4 @@
 <?php
-function checkIfSetAndString($var) {
-	if (isset($var) && is_string($var)) {
-		return true;
-	} else {
-		return false;
-	}
-}
-function returnError($message) {
-	return "Unsuccessful: ".$message;
-}
 function returnSuccess($message) {
 	return "Successful: ".$message;
 }
