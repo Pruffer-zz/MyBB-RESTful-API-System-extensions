61c08374b04a4a308b3e0d291753abc84c4133f5
Prüffer <pruffer@avantheim.org>
2020-11-28 13:45:16 +0100

    added functions from jsonfunctions.php

--- Upload/inc/plugins/restfulapi/functions/varfunctions.php
+++ Upload/inc/plugins/restfulapi/functions/varfunctions.php
@@ -20,5 +20,19 @@ function checkIfSetAndNumerical($var) {
 		return false;
 	}
 }
+function checkIfJson($body) {
+	if ($return = json_decode($body)) {
+		return $return;
+	} else {
+		return false;
+	}
+}
+function getKeyValue($key, $body) {
+	if ($returnKey = $body->$key) {
+		return $returnKey;
+	} else {
+		return false;
+	}
+}
 ?>
 
