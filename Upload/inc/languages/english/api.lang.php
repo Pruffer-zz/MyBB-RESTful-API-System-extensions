2dece8432d413da97d103329e56502c71b0a9ddb
Prüffer <pruffer@avantheim.org>
2020-11-28 21:09:47 +0100

    added lang file

---
+++ Upload/inc/languages/english/api.lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * MyBB-RESTful-API-System-extensions language file
+ * Released by Prüffer (avantheim.org) under GNU GPLv3
+ */
+
+$l['api_key_missing'] = 'JSON key(s) potentially missing: ';
+$l['api_key_contains_directory'] = 'JSON key(s) contain a directory: ';
+$l['api_directory_traversal_failed'] = 'Directory traversal check failed, or location doesn\'t exist.';
+$l['api_directory_is_file'] = 'Specified directory is a file';
+$l['api_file_missing'] = 'File missing: ';
+$l['api_file_write_failed'] = 'File write failed';
+$l['api_file_read_failed'] = 'File read failed';
+$l['api_file_is_directory'] = 'File is a directory';
+$l['api_file_or_directory_exists'] = 'Directory / file already exists';
+$l['api_directory_write_failed'] = 'Directory write failed';
+$l['api_incorrect_content_type'] = 'Missing or incorrect content-type header. Required content-type: ';
+$l['api_no_valid_action_specified'] = 'No valid action specified in the URL';
+$l['api_json_invalid'] = 'Invalid JSON data';
+$l['api_json_read_error'] = 'Unable to read JSON data';
